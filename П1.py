e2dec96132268e33568d4f57d2cf1fd31ecd8c41
ZubovAA <Zuboa2801@gmail.com>
2022-08-17 16:01:34 +0700

    Добавления экрана в приложение

--- П1.py
+++ П1.py
@@ -1,7 +1,19 @@
 from tkinter import *
+import math
 
 root = Tk()
 root.title("Научный калькулятор")
 root.geometry("500x300")
+
+# разбивка экрана на части с помощью надписей с текстом
+
+# создание экрана приложения
+
+screen = LabelFrame(root, text="Экран")
+screen.pack()
+
+screen_text = Entry(screen, width=50, relief=GROOVE, font="Arial 16", justify=RIGHT, state=DISABLED)
+screen_text.pack()
+
 
 root.mainloop()
